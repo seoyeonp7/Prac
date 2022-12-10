8140440c2239ea3d3c3f4da5a2b423cb4e9f980d
seoyeonp7 <sy410y@naver.com>
2022-12-10 23:54:10 +0900

    divisorTest

---
+++ Prac1/src/test/DivisorTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.*;
+
+public class DivisorTest {
+	public static void main(String[] args) {
+        int answer = 0;
+        int n = 0;
+        int sqrt = (int)Math.sqrt(n);
+        List<Integer> list = new ArrayList();
+
+        for(int i=1;i<=sqrt;i++){
+            if(n%i==0){
+                list.add(i);
+                if(n/i!=i){
+                    list.add(n/i);
+                }
+            }
+        }
+//        Collections.sort(list);
+        answer = list.stream().mapToInt(i->i).sum();
+
+    }
+}
